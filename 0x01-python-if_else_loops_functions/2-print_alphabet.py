c4c8016a6ff92cbe34b06f0ac05c809d3ce3b455
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-12 04:22:32 +0000

    2-print_alphabet.pyupdated

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 
 # ASCII values for lowercase letters range from 97 to 122
 
 for i in range(97, 123):
-    print(chr(i), end='')
+    print('{:c}'.format(i), end='')
