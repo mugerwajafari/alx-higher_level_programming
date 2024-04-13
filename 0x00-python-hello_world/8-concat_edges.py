97aca2f49681141acdd5bba220cad7003bff6621
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-13 18:21:55 +0000

    8-concat_edges.py update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print("object-oriented programming with Python")
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[0:6]
+print(str)
