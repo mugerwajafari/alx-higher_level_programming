2eba664009d8359d223f0c51438bbbbd29433fa5
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-13 15:39:43 +0000

    3-print_number.py updated

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
-# Prompt the user to enter a number
-number = input("Enter a number: ")
-
-# Print the integer stored in the variable number, followed by "Battery street", with automatic newline
-print(str(number) + " Battery street")
-
+number = 98
+print("{} Battery street".format(number))
