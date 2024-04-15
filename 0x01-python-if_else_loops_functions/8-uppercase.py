7e2997bcf64dda8abaddbbb9594afe93d7d31268
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-15 12:51:49 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 
-def uppercase(word):
-    for char in word:
+def uppercase(str):
+    for char in str:
         if not (ord('A') <= ord(char) <= ord('Z')):
             return False
     return True
-
-
-word = input("Enter a word: ")
-if uppercase(word):
-    print("The word is all uppercase.")
-else:
-    print("The word contains non-uppercase characters.")
