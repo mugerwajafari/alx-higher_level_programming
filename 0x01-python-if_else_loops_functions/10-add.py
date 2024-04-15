2d64cff26d258b22718e83f4d13e941a10d7fa80
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-15 14:21:27 +0000

    10-add.py debug

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
-# Taking user input for two numbers
-
-a = float(input("Enter the first number: "))
-b = float(input("Enter the second number: "))
-
-
 def add(a, b):
     return a + b
 
