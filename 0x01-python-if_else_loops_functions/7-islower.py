bcf4044db76920688c3877f1d0f973fbc891a4ea
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-15 12:39:04 +0000

    7-islower.py updated

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def islower(c):
-    if ord(c) >= 97 and ord(c) <= 122 and  len(c) == 1:
+    if ord(c) >= 97 and ord(c) <= 122 and len(c) == 1:
         return True
     else:
         return False
