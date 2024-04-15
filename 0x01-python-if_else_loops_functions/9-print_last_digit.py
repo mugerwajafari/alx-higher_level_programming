4b07798a36da337da00314c6dbf4d60d3299d9b2
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-15 14:17:17 +0000

    9-print_last_digit.py updated

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -8,9 +8,3 @@ def print_last_digit(number):
 
     print(last_digit, end='')
     return last_digit
-
-
-user_input = input("Enter a number: ")
-number = int(user_input)
-last_digit = print_last_digit(number)
-print("\nThe last digit of the number is:", last_digit)
