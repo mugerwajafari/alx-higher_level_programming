a7dcd2b423fa8e107ffd969ec635801ef6e5c382
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-15 13:21:44 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 def uppercase(s):
     for char in s:
-        print(chr(ord(char) - 32), end='')
+        print("{:s}".format(chr(ord(char) - 32)), end='')
     print()
 
 # Test the function
 uppercase("hello")
+
