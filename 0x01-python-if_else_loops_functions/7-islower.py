76948d7d4b1a5061cfab722395a8aa70c658a042
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-15 12:36:47 +0000

    7-islower.py updated

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 
 def islower(c):
-    return ord(c) >= 97 and ord(c) <= 122
-
-
-c = input("Enter a character: ")
-
-if len(c) == 1:
-    if islower(c):
-        print(True)
+    if ord(c) >= 97 and ord(c) <= 122 and  len(c) == 1:
+        return True
     else:
-        print(False)
-else:
-    print("Please enter exactly one character.")
+        return False
