882ea19ad2d19a0edacf18615b279f1b070a262e
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-16 21:46:46 +0000

    0-add.py updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,22 +1,7 @@
-"""
-Write a program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3
-
-You have to use print function with string format to display integers
-You have to assign:
-the value 1 to a variable called a
-the value 2 to a variable called b
-and use those two variables as arguments when calling the functions add and print
-a and b must be defined in 2 different lines: a = 1 and another b = 2
-Your program should print: <a value> + <b value> = <add(a, b) value> followed with a new line
-You can only use the word add_0 once in your code
-You are not allowed to use * for importing or __import__
-Your code should not be executed when imported - by using __import__, like the example below
-"""
 #!/usr/bin/python3
-from add_0.py import def add
-#block import and write your own cord 
+
+import add_0 
 if __name__ == "__main__":
-  #  asign varible and print
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+	answer = add_0.add(1,2)
+	print("{:d}".format(answer))
+
