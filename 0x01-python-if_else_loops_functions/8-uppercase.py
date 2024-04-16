21dfc5a8964141074aabcbb09ac640f8000d93ef
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-16 13:53:21 +0000

    8-uppercase.py updated

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def uppercase(s):
-    result = ""
-    for char in s:
-        if char.isalpha():  # Check if the character is an alphabet letter
-            result += "{:s}".format(chr(ord(char) - 32))
-        else:
-            result += "{:s}".format(char)  # Add non-alphabet characters as is
-    print("{}".format(result), end="")  # Print the final result without a newline
-    print()
+    """Convert to uppercase."""
+    for c in s:
+        ch = ord(c)
+        if ch >= 97 and ch <= 122:
+            ch -= 32
+        print(chr(ch), end="")
+    print("")
