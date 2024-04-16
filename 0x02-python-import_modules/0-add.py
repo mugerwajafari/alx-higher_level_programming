69a60900da8b99a48a54989f7abf1ace61661b4b
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-16 22:23:46 +0000

    0-add.py updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
-from add_0 import add 
-
+from add_0 import add
 if __name__ == "__main__":
-	a = 1
-	b = 2
-	answer = add(a,b)
-
-	print("{:d} +{:d} = {:d}".format(a,b,answer))
-
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
