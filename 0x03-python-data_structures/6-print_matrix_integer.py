c59a669b9b24d5504b23a4b4fa76c20031252054
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-18 17:36:30 +0000

    6-print_matrix_integer.py updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # Iterate through each row in the matrix
-    for row in matrix:
-        # Iterate through each element in the row
-        for element in row:
-            # Use str.format() to print each element without casting it into a string
-            # Separate elements by space and ensure they are right-aligned with width 2
-            print("{:d}".format(element), end=" ")
-        # Print a new line after each row
-        print()
+    if matrix:
+        for elems in matrix:
+            i = 1
+            length = len(elems)
+
+            for elem in elems:
+                if i == length:
+                    print('{:d}'.format(elem), end='')
+                else:
+                    print('{:d}'.format(elem), end=' ')
+                i += 1
+
+            print()
