784250cdfb0c4804c1c68e1a511b7024d02fa488
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-18 16:04:23 +0000

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,22 +1,14 @@
-#!/bin/bash/python3
-'''
-Write a function that retrieves an element from a list like in C.
-
-Prototype: def element_at(my_list, idx):
-If idx is negative, the function should return None
-If idx is out of range (> of number of element in my_list), the function should return None
-You are not allowed to import any module
-You are not allowed to use try/except
-'''
+#!/bin/python3
 
 def element_at(my_list, idx):
-    #ensuring not empty for two var
     if idx < 0 or idx >= len(my_list) :
         return "None"
     else:
         return my_list[idx]
-    #checking
-my_list = [10, 20, 30, 40, 50]
-idx = 2
-result = element_at(my_list, idx)
-print(idx, result)
+
+
+my_list = [1, 2, 3, 4, 5]
+
+idx = 3
+element_at(my_list, idx)
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
