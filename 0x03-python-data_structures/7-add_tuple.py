c351eb7d34273741512959afb37f49e4f354087e
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-18 17:43:52 +0000

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 def add_tuples(tuple1, tuple2):
     # Check if the tuples have the same length
@@ -10,11 +10,3 @@ def add_tuples(tuple1, tuple2):
     for i in range(len(tuple1)):
         result = result[:i] + (result[i] + tuple2[i],) + result[i+1:]
     return result
-
-# Example usage
-tuple1 = (1, 2, 3)
-tuple2 = (4, 5, 6)
-result = add_tuples(tuple1, tuple2)
-print("The sum of", tuple1, "and", tuple2, "is", result)
-
-
