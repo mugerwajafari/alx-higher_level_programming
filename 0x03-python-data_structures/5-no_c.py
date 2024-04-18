33ded5c4aca659e61c222c37c00318d7d3d6f750
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-18 17:24:53 +0000

    5-no_c.py updated

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,22 +1,20 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 def no_c(my_string):
-    # Ensure the code contains c
-    if 'c' in my_string.lower() or 'C' in my_string:
-        # Prompt for the new letter
-        new_letter = input("Enter the letter to replace 'c' and 'C' with: ")
-        # Replace 'c' and 'C' with the new letter
-        new_string = ''
-        for char in my_string:
-            if char != 'c' and char != 'C':
-                new_string += char
-            else:
-                new_string += new_letter
-        return new_string
-    else:
-        return "The word does not contain the letter 'c' or 'C'."
+    """
+    Removes all occurrences of 'c' and 'C' from the input string.
 
-my_string = input("Enter a word with 'c' or 'C': ")
-answer = no_c(my_string)
-print("The new word is:", answer)
+    Parameters:
+    my_string (str): The input string from which 'c' and 'C' are removed.
+
+    Returns:
+    str: The modified string with 'c' and 'C' removed.
+    """
+    new_str = ''
+
+    for i in my_string:
+        if i not in ('C', 'c'):  # Using 'not in' for better readability
+            new_str += i
+
+    return new_str
 
