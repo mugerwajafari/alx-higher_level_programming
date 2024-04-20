2272602df93e5aeed19e59aef78bb5ccb2d1f9c3
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-20 20:35:12 +0000

    9-multiply_by_2.py updated

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     # Create a copy of the input dictionary to avoid modifying the original dictionary
     b_dictionary = a_dictionary.copy()
@@ -10,11 +10,3 @@ def multiply_by_2(a_dictionary):
 
     # Return the modified dictionary with values multiplied by 2
     return b_dictionary
-
-# Example usage:
-original_dictionary = {'a': 1, 'b': 2, 'c': 3}
-print("Original dictionary:", original_dictionary)
-
-multiplied_dictionary = multiply_by_2(original_dictionary)
-print("New dictionary with values multiplied by 2:", multiplied_dictionary)
-
