627a64b80be61c9d32611f8584ef871f05ace0ec
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-20 20:39:19 +0000

    9-multiply_by_2.py updated

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def multiply_by_2(a_dictionary):
     # Create a copy of the input dictionary to avoid modifying the original dictionary
     b_dictionary = a_dictionary.copy()
