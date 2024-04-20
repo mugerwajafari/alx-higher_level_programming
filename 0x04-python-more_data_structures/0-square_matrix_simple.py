1fc3ab7818228b48d03258b486a1f354ce89c417
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-20 06:39:30 +0000

    0-square_matrix_simple.py updated

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    for i_m in range(len(matrix)):
-        row = matrix[i_m]  # Fix: Get the row from the matrix
-        squared_row = [x ** 2 for x in row]  # Square each element in the row
-        print("row", i_m + 1, ':', squared_row)
+    new_matrix = []
 
-    print("no more rows")
+    if len(matrix) > 0:
+        for elems in matrix[:]:
+            new_matrix.append(list(map(lambda x: x ** 2, elems)))
+
+    return new_matrix
