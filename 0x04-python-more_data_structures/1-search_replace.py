843c29d8fc74bd3f4b47dee1ac18bd519f7c1823
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-20 06:44:06 +0000

    1-search_replace.py  updated

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
     # Loop through the list using index-based iteration
     for i in range(len(my_list)):
@@ -6,13 +6,4 @@ def search_replace(my_list, search, replace):
         if my_list[i] == search:
             # Replace the element with the new value
             my_list[i] = replace
-    # Return the modified list
     return my_list
-
-# Example usage
-my_list = [1, 2, 3, 4]
-search = 2
-replace = 8
-# Call the function and print the modified list
-print(search_replace(my_list, search, replace))
-
