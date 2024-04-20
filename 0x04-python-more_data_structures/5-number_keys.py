1a76cc36a2587b69927915b6e0e49eff57d5a5b5
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-20 18:08:50 +0000

    5-number_keys.py updated

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,8 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
+
 def number_keys(a_dictionary):
     return len(a_dictionary.keys())
-
-# Example usage
-my_dictionary = {"a": 1, "b": 2, "c": 3}
-print(number_keys(my_dictionary))
-
