314444b5a86ab267a76dc17b11cdf83623929418
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-20 20:17:33 +0000

    7-update_dictionary.py updated

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
+
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
-
-# Define the dictionary 'a_dictionary'
-a_dictionary = {'cow': 'moo'}
-
-# Define the key and value to be updated
-key = 'dog'
-value = 'bark'
-
-# Call the function and print the updated dictionary
-print(update_dictionary(a_dictionary, key, value))
