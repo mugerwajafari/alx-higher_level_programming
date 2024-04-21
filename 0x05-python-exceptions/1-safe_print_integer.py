fa71ffcaff16db2ea0e8f36ccf4ea0c4b2e1e906
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-21 14:42:25 +0000

    1-safe_print_integer.py updated

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True  # Return True if value is successfully printed
+        return True  # Return True if value is successful
     except (ValueError, TypeError):
-        return False  # Return False if value cannot be formatted as an integer or if an error occurs
+        return False 
