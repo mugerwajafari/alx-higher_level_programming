349d96ed26352191cdae29343835ebc495b19b4c
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-27 10:21:42 +0000

    4-square.py updated

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Square Class with an init """
+"""Lets break down the property addition"""
 
 
 class Square:
@@ -9,10 +9,13 @@ class Square:
 
     """
 
-    def __init__(self,size):
+    def __init__(self, size=0):
         """__init__
 
+        The __init__ method initializes the size value of the square.
+
         Attributes:
+            size (:obj:`int`, optional): The size of the square.
 
         Raises:
             TypeError: If `size` type is not `int`.
@@ -23,17 +26,17 @@ class Square:
 
         if type(size) is not int:
             raise TypeError('size must be an integer')
+
         if size < 0:
             raise ValueError('size must be >= 0')
 
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
         return self.__size
 
     @size.setter
-   
     def size(self, size):
         if type(size) is not int:
             raise TypeError('size must be an integer')
