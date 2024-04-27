cf2940640e3ba38f3d8f33695cecb9fc8cc75734
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-27 11:24:25 +0000

    5-square.py updated

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,15 +3,25 @@
 
 
 class Square:
-    """Square class
+    """Square Class
+
+    A Square Class
 
     """
 
-    def __init__(self,size=0):
-        """
+    def __init__(self, size=0):
+        """__init__
+
+        The __init__ method initializes the size value of the square.
+
+        Attributes:
+            size (:obj:`int`, optional): The size of the square.
+
         Raises:
-        if size not int raise TypeError
-        if size < ValueError
+            TypeError: If `size` type is not `int`.
+
+            ValueError: If `size` is less than `0`.
+
         """
 
         if type(size) is not int:
@@ -21,23 +31,39 @@ class Square:
             raise ValueError('size must be >= 0')
 
         self.size = size
-       
+
     @property
     def size(self):
         return self.__size
 
     @size.setter
-    def size(self, value):
+    def size(self, size):
+        """__init__
+
+        The size setter method update the size value of the square.
+
+        Attributes:
+            size (:obj:`int`): The new size of the square.
+
+        Raises:
+            TypeError: If `size` type is not `int`.
+
+            ValueError: If `size` is less than `0`.
+
+        """
+
         if type(size) is not int:
             raise TypeError('size must be an integer')
 
         if size < 0:
             raise ValueError('size must be >= 0')
-         
-        self.size = size
-    
+
+        self.__size = size
+
     def area(self):
-        """Returns the current square area         """
+        """Returns the current square area
+
+        """
         return self.__size ** 2
 
     def my_print(self):
