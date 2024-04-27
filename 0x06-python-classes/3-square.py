03c1a800f0e701bfc55b4b0fe54edb3a6137b33c
Mugerwa Jafari <mugerwajafari@gmail.com>
2024-04-27 09:46:07 +0000

    3-square.py updated

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,14 +5,8 @@ class Square:
     A Square Class
 
     """
-
     def __init__(self, size=0):
         """__init__
-
-        The __init__ method initializes the size value of the square.
-
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
 
         Raises:
             TypeError: If `size` type is not `int`.
